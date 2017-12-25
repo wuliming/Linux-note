fba25535f482966be0f3e689406e36baab7c26f1
wulm <wulm.fnst@cn.fujitsu.com>
2017-12-25 10:48:17 +0800

    add some knowhow

--- I_tool/process/watch_proc.sh
+++ I_tool/process/watch_proc.sh
@@ -1,5 +1,5 @@
 # !/bin/sh
 INTERVAL=$1
 GREP=$2
-watch -d -n ${INTERVAL} "ps -eo time,pid,ppid,%cpu,%mem,psr,comm,args | grep -v grep | grep ${GREP}"
+watch -d -n ${INTERVAL} "ps -eo time,pid,ppid,%cpu,%mem,psr,rss,vsz,size,comm,args | grep -v grep | grep ${GREP}"
 
