2ee6e83508bc63d94bf631ca7cd18e9ac5ff0218
wulm <wulm.fnst@cn.fujitsu.com>
2018-03-23 15:53:48 +0800

    20180323 modify

--- I_tool/get_hardware.sh
+++ I_tool/get_hardware.sh
@@ -7,16 +7,42 @@ dmidecode -t system | grep -i Product
 dmidecode -t chassis | grep -i Version
 
 echo "Processor info:"
+Siblings=`grep "siblings" /proc/cpuinfo | uniq | awk '{print $3}'`
+Cores=`grep "cpu cores" /proc/cpuinfo | uniq | awk '{print $4}'`
+# when machine is PC, it's invalid
+if [ ${Siblings} = ${Cores} ]; then
+	echo -e "\tHT:\tOFF"
+else
+	echo -e "\tHT:\tON"
+fi
 dmidecode -t processor | grep -iE "version|speed|Core" | grep -v Multi | sort -u
+# generally, when "Thread(s) per core:    2"   -> HT_ON
+# "Thread(s) per core:    1"  		       -> HT_OFF
+lscpu | awk '{printf("\t%s\n", $0)}'
+numactl -H | awk '{printf("\t%s\n", $0)}'
 
 echo "Disk Info:"  
-parted -l|grep 'Disk /dev/sd'|awk -F ',' '{print "  ",$1}'  
+parted -l | grep -Ei "Disk /dev/sd|sector size" | awk -F ',' '{printf("\t%s\n",$1)}' | sort -u
+dmesg | grep sd* | grep -i "write cache" | awk \
+'{if(match($8, "enabled")) printf("\t%s is write back\n", $5); \
+else printf("\t%s is write through\n", $5)}'  2>/dev/null
 
 echo "Network Info:"  
-lspci | grep Ethernet 
+lspci | grep Ethernet | awk '{printf("\t%s\n", $0)}'
+for eth in  `ip a | grep -E "^[1-9]" | awk -F': |:' '{print $2}'`
+do
+	ethtool $eth 
+done | grep -iE "Settings|Speed|Duplex|Auto-negotiation|detected" | awk '{printf("\t%s\n", $0)}'
  
 echo "Memory Info:"  
 #dmidecode | grep -A5 "Memory Device" |grep -i "  *Size" |grep -v "No"
+echo -e "\tMemory number:\t"`dmidecode | grep -A5 "Memory Device" | grep -i "  *Size" | grep -v No |wc -l`
 dmidecode -t memory | grep -iE "type:|size|Speed|Manufacturer" | grep -Ev "No|Unknown|Error"
-echo "Memory number:"`dmidecode | grep -A5 "Memory Device" | grep -i "  *Size" | grep -v No |wc -l`
+
+echo "Power Info:"
+cpupower frequency-info | awk '{ printf("\t%s\n", $0) }'
+cpupower -c all idle-info | awk '{ printf("\t%s\n", $0) }'
+
+echo "IRQ affinity Info:"
+find /proc/irq -name smp_affinity_list -exec echo {} \; -exec cat {} \;  | awk '{ printf("\t%s\n", $0) }'
 
--- network/tool/get_interrupts.sh
+++ network/tool/get_interrupts.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/sh 
 
 #EX: ./get_interrupts.sh 0201075607/log-server/lenth-256K/round-1/tazyu-1/0201075533/getperfinfo/interrupts_0201075533  enp4s0f0 12
 #$1		file directory
 #$2		NIC name: eth1
 #$3		Cores or the max X of eth-RxTx-$X
+#$4		0: kvm  1: Native
 
 LANG=C
 export LANG
-
 PATH="/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export PATH
-
 FILE=$1
 NIC=$2
 if [ -z $3 ];then
@@ -19,12 +18,12 @@ else
 	CORES=$3
 fi
 
-FILE=`find $1 -name "interrupt*"`
+#FILE=`find $1 -name "interrupt*|*irq*"`
 echo ${FILE}
 do_get_interrupt(){
-	cores=${CORES} && cat ${FILE} | \
+	cat ${FILE} | \
 	grep "${1}$"  | \
-	awk 'BEGIN {
+	awk -v cores=${CORES}  'BEGIN {
 			interrupt_no=0;
 			sum=0;
 			start[cores];
@@ -32,29 +31,65 @@ do_get_interrupt(){
 		}
 		{
 			if(NR==1){ 
-				for(i=0; i<12; i++) {
+				for(i=0; i <cores; i++) {
 					start[i]=$(i+2);
 				}
 				interrupt_no=$1;
-				dev=$NF
+				dev=$NF;
 			}
 		}
 		END {
-			for(i=0; i<NR; i++) {
+			for(i=0; i<cores; i++) {
 				end[i]=$(i+2);
 				if( end[i]-start[i] > 0 ) {
-					printf("interupt NO.: %-4d\tcpu: %-3d\t interrupts: %-10d\t device: %s\n", \
-						 interrupt_no, i, end[i]-start[i], dev);
+					printf("interupt NO.: %-4d\tcpu: %-3d\t interrupts: %-10d\t device: %s\t %d -> %d\n", \
+						 interrupt_no, i, end[i]-start[i], dev, start[i], end[i]);
 				}
 			}
 		}'
 }
 
+do_get_interrupt_all(){
+	cat ${FILE} | \
+	grep -E "*${1}*"  | \
+	awk -v cores=${CORES}  'BEGIN {
+			interrupt_no=0;
+			sum=0;
+			start[][cores];
+			end[][cores];
+		}
+		{
+			if(NR==1){ 
+				for(i=0; i <cores; i++) {
+					start[i]=$(i+2);
+				}
+				interrupt_no=$1;
+				dev=$NF;
+			}
+		}
+		END {
+			for(i=0; i<cores; i++) {
+				end[i]=$(i+2);
+				if( end[i]-start[i] > 0 ) {
+					printf("interupt NO.: %-4d\tcpu: %-3d\t interrupts: %-10d\t device: %s\t %d -> %d\n", \
+						 interrupt_no, i, end[i]-start[i], dev, start[i], end[i]);
+				}
+			}
+		}'
+}
 declare -i i=0
 
 do_get_interrupt ${NIC} 
+#lscpu | grep Hypervisor
+flag=$4
+if [[ $flag = 0 ]]; then
+	do_get_interrupt ${NIC}-tx-${i}
+	do_get_interrupt ${NIC}-rx-${i}
+	#do_get_interrupt_all ${NIC}
+else
+	for((i; i<${CORES}; i++))
+	do
+		do_get_interrupt ${NIC}-TxRx-${i}
+	done
 
-for((i; i<${CORES}; i++))
-do
-	do_get_interrupt ${NIC}-TxRx-${i}
-done
+fi
